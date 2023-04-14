970eaae5824654842b2feb17619094f783306ab7
Slayer2084 <oeg2084@oegym.de>
2023-04-14 13:26:34 +0200

    changed cnbc's download delay

--- NewsArticlesScraper/Scrapers.py
+++ NewsArticlesScraper/Scrapers.py
@@ -19,7 +19,7 @@ class CNBCSpider(scrapy.Spider):
         'USER_AGENT': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 '
                       'Safari/537.1',
         'ROBOTSTXT_OBEY': False,
-        'DOWNLOAD_DELAY': 0.5,
+        'DOWNLOAD_DELAY': 6,
         # 'JOBDIR': './News/CNBCJobs',
         'REQUEST_FINGERPRINTER_IMPLEMENTATION': '2.7',
         'DOWNLOADER_MIDDLEWARES': {

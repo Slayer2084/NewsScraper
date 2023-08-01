7fdf3efd27258383165c22679725534ceffc743e
Slayer2084 <oeg2084@oegym.de>
2023-08-01 16:15:36 +0200

    increased retry times

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.2.7.3'
+VERSION = '0.2.7.4'
 DESCRIPTION = 'Scraping news articles'
 LONG_DESCRIPTION = 'A package that allows you to scrape news articles from various news sites via scrapy.'
 

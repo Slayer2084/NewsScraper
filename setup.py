8d093ccfe95ce9f3b5a31d6accf57937a387bdab
Slayer2084 <oeg2084@oegym.de>
2023-10-12 01:40:31 +0200

    fix to NytUrlSpider

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.2.7.5'
+VERSION = '0.2.7.6'
 DESCRIPTION = 'Scraping news articles'
 LONG_DESCRIPTION = 'A package that allows you to scrape news articles from various news sites via scrapy.'
 
